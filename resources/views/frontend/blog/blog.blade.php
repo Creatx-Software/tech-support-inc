bc4e0bf3c53d6b676c67ec0378bfe57780099b85
Thisara Ariyawansha <thisara.a2001@gmail.com>
2026-08-18 13:53:45 +0530

    update blog page

--- resources/views/frontend/blog/blog.blade.php
+++ resources/views/frontend/blog/blog.blade.php
@@ -1,23 +1,4 @@
-        <section class="jarallax text-light relative rounded-1 overflow-hidden mt-80 mt-sm-70 mx-2">
-            <div class="de-gradient-edge-top"></div>
-            <img src="/assets/images/background/1.webp" class="jarallax-img" alt="">
-            <div class="container relative z-2">
-                <div class="row justify-content-center">
-                    <div class="col-lg-6 text-center">
-                        <div class="subtitle id-color wow fadeInUp mb-2">IT Insights &amp; Updates</div>
-                        <div class="clearfix"></div>
-                        <h2 class="fs-60 fs-xs-8vw wow fadeInUp" data-wow-delay=".4s">Blog</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="crumb-wrapper">
-                <ul class="crumb">
-                    <li><a href="index.html">Home</a></li>
-                    <li class="active">Blog</li>
-                </ul>
-            </div>
-            <div class="sw-overlay op-8"></div>
-        </section>
+
 
         <section class="bg-light">
             <div class="container">
